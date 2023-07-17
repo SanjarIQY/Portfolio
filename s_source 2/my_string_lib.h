ef2fe859e31c2dd407d57ee4aae6fcd99089c2a3
SanjarIQY <102430078+SanjarIQY@users.noreply.github.com>
2023-07-17 17:02:48 +0500

    needed files to s_bc.c

---
+++ s_source 2/my_string_lib.h
@@ -0,0 +1,7 @@
+int my_strlen(char* string);//length of the string
+void my_strcpy(char* dest, char* source);
+void my_strncpy(char* dest, char* source, int size);
+void fill_with_null(char* string, int size);
+void split_gaps(char* task);
+int power(int num, int pw);
+int char_to_dec(char* str);
