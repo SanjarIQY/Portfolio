902a8c6e6b197e11b24ecab73774eb1a064142e1
SanjarIQY <102430078+SanjarIQY@users.noreply.github.com>
2023-07-26 09:44:19 +0500

    Delete s_source 2 directory

--- s_source 2/check_functions.h
+++
@@ -1,8 +0,0 @@
-int is_num(char p1);
-int check_for_parantheses_error(char* task);
-int check_for_error(char* task);
-int counting_equals(char* task);
-int check_for_divide_error(char* task);
-int check_for_char_error(char* task);
-int check_for_one_number(char* task);
-int check_for_minus(char* task, int index);
--- s_source 2/my_string_lib.h
+++
@@ -1,7 +0,0 @@
-int my_strlen(char* string);//length of the string
-void my_strcpy(char* dest, char* source);
-void my_strncpy(char* dest, char* source, int size);
-void fill_with_null(char* string, int size);
-void split_gaps(char* task);
-int power(int num, int pw);
-int char_to_dec(char* str);
